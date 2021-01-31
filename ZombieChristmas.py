7993910a0a536c2d7b1e08bb494a8ab4ccda30c5
basicaly <60189804+basicaly@users.noreply.github.com>
2021-01-31 17:58:45 +0100

    Update ZombieChristmas.py

--- ZombieChristmas.py
+++ ZombieChristmas.py
@@ -1,14 +1,65 @@
 import random
+import matplotlib.pyplot as plt
+from celluloid import Camera
 #the list positions contains the characters and on which square they are: positions = [["HH", 17], ["Z", 2] ...]
 #each position identifies a unique square on a column of given rows and columns, as you see in the function updatePosition. 
 #so the grid has the appearance as follows:
 # if it has 3 columns and 2 rows: 0 1 2
 #                                 3 4 5
 #identifying each square with a number.
+
+def splitup(positions):
+    Z = []
+    ZH = []
+    H = []
+    HH = []
+    for i in positions:
+        if i[0] == "Z":
+            Z.append(i)
+        if i[0] == "ZH":
+            ZH.append(i)
+        if i[0] == "H":
+            H.append(i)
+        if i[0] == "HH":
+            HH.append(i)
+    return Z, ZH, H, HH
+
+
+def getcoordinates(positions, rows, columns):
+    Z, ZH, H, HH = splitup(positions)
+    coordinate = [[] for i in range(4)]
+    rows, columns = rows - 1, columns - 1
+    for z in Z:
+        coordinate[0].append(((z[1] % columns), (z[1] // columns)))
+    for zh in ZH:
+        coordinate[1].append(((zh[1] % columns), (zh[1] // columns)))
+    for h in H:
+        coordinate[2].append(((h[1] % columns), (h[1] // columns)))
+    for hh in HH:
+        coordinate[3].append(((hh[1] % columns), (hh[1] // columns)))
+    return coordinate
+
+def splitter(precoordinates):
+    Zc, ZHc, Hc, HHc = [[], []], [[], []], [[], []], [[], []]
+    for i in range(len(precoordinates)):
+        for l in precoordinates[i]:
+            if i == 0:
+                Zc[0].append(l[0])
+                Zc[1].append(l[1])
+            elif i == 1:
+                ZHc[0].append(l[0])
+                ZHc[1].append(l[1])
+            elif i == 2:
+                Hc[0].append(l[0])
+                Hc[1].append(l[1])
+            elif i == 3:
+                HHc[0].append(l[0])
+                HHc[1].append(l[1])
+    return Zc, ZHc, Hc, HHc
+
+
 #the function updatePosition updates for every entry in the list positions, the square number, randomly, one step either up or down, left or right.  
 #At the edges there is a snake thing going on where they reappear on the other end.
-
-
 def updatePosition(rows, columns, position, rnd):
     position = position + 1
     if 0 <= rnd < 0.25:  # Right
@@ -118,47 +169,59 @@ def bestand(positions):
 # this is a function to test the code. i can set the world grid. number of rows and columns, and how many characters i want on my grid, then return a list positions
 # which is the one used throughout the whole simulation
 def randomPositions(rows, columns, population):
-    limit = rows * columns
     positions = []
     index = 0
-    while index < population:
-        positions = 1 // random.random()
-        if positions > limit:
-            continue
-        positions.append([random.random(), int(positions)])
-        index += 1
+    for i in range(population):
+        positions.append([random.random(), int(random.uniform(0, rows * columns))])
     for index in range(len(positions)):
         if 0 <= positions[index][0] <= 0.25:
             positions[index][0] = 'H'
         elif 0.25 < positions[index][0] <= 0.5:
             positions[index][0] = 'ZH'
-        elif 0.5 < positions[index][0] <= 0.6:
+        elif 0.5 < positions[index][0] <= 0.51:
             positions[index][0] = 'HH'
-        elif 0.6 < positions[index][0] < 1:
+        elif 0.51 < positions[index][0] < 1:
             positions[index][0] = 'Z'
     return positions
 
 # this is the heart of the simulation, the game loop. while the function at line 81 is returning False, the game is running. once the christmas fate is True it exits the loop
-# and prints out the christmas fate
+# and prints out the christmas fate. now it has also the animation funciton which creates a gif with the animation.
 def zombieChristmas(rows, columns, positions):
-    intermediate = [] #intermediate list to store the positions
+    intermediate = []
+    fig = plt.figure()
+    camera = Camera(fig)
+    counter = 0
     while not christmasFated(positions):
-        updatePositions(rows, columns, positions) #updates the position of every character on the grid randonmly
-        sortPositions(positions) #sorts the position in ascnedin order
-        while positions: 
-            square = extractSquare(positions)#i pop the characters form the list positions that are on the same square
-            giftExchange(square) #here we apply the rules of the game
-            mergeSquare(square, intermediate) #here i return the square to the intermediary list so as no to lose the positino of the characters
-            if not positions: #once the position list empty, i assign to the positions the intermediary list and the loop runs again.
+        counter += 1
+        precoordinates = getcoordinates(positions, rows, columns)
+        Zc, ZHc, Hc, HHc = splitter(precoordinates)
+        plt.scatter(Zc[0], Zc[1], c = 'r')
+        plt.scatter(ZHc[0], ZHc[1], c = 'b')
+        plt.scatter(Hc[0], Hc[1], c = 'g')
+        plt.scatter(HHc[0], HHc[1], c = '#1f77b4')
+        camera.snap()
+        updatePositions(rows, columns, positions)
+        sortPositions(positions)
+        if counter > 1000:
+            break
+        while positions:
+            square = extractSquare(positions)
+            giftExchange(square)
+            mergeSquare(square, intermediate)
+            if not positions:
                 positions = intermediate
                 intermediate = []
                 break
+    print('making animation', counter, 'frames')
+    animation = camera.animate()
+    animation.save('animation.gif', writer = 'pillow')
     return christmasFate(positions)
 
+
 #number of rows and columns on the world grid
-rows, columns = 15, 15
+rows, columns = 5, 5
 #how many characters there are on the grid
-population = 40
+population = 10
 #creating a random list of characters wiht associated positions
 positions = randomPositions(rows, columns, population)
 #running the game loop
